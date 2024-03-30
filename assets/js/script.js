cfdf1fa384de40f803fe8842ec92e5dc91c73c85
Mamta Paswan <mamtapaswan2021@gmail.com>
2024-03-30 14:46:54 +0530

    Change search form and change font family

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,7 @@ modeToggleBtn.addEventListener("click", toggleColorMode);
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     const headers = document.querySelector("header");
     function handleScroll() {
         if (window.scrollY > 0) {
@@ -43,19 +43,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // mobile menu code ============ start =====>
-    // let searchIcon = document.querySelector(".search-icon");
-    // let searchForm = document.querySelector(".search-form");
-    // let svg1 =
-    //     '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M7.333 12.667A5.333 5.333 0 1 0 7.333 2a5.333 5.333 0 0 0 0 10.667ZM14 14l-2.9-2.9" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
-    // let svg2 =
-    //     '<svg fill="#fff" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
-    // let isSvg1 = true;
-    // searchIcon.addEventListener("click", function () {
-    //     searchIcon.innerHTML = isSvg1 ? svg2 : svg1;
-    //     isSvg1 = !isSvg1;
+    let searchIcon = document.querySelector(".mob-search-btn");
+    let searchForm = document.querySelector(".search-form");
+    let svg1 =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none"><path d="M7.333 12.667A5.333 5.333 0 1 0 7.333 2a5.333 5.333 0 0 0 0 10.667ZM14 14l-2.9-2.9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+    let svg2 =
+        '<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
+    let isSvg1 = true;
+    searchIcon.addEventListener("click", function () {
+        searchIcon.innerHTML = isSvg1 ? svg2 : svg1;
+        isSvg1 = !isSvg1;
 
-    //     searchForm.classList.toggle("search-bar-show");
-    // });
+        searchForm.classList.toggle("search-bar-show");
+    });
 
     const toggleslideBtn = document.querySelector(".toggle-slide-btn");
     const cancelBtn = document.querySelector(".cancel-btn");
